b7da707d97875d9f53f8b7b0f64bef3f828764fc
Vishal Dogra <111854114+VISHAL-099@users.noreply.github.com>
2025-10-28 12:00:55 +0530

    Added hello_world_vishal.py for Hacktoberfest 2025

--- hello_world/Hello-World-2025/hello_world_vishal.py
+++ hello_world/Hello-World-2025/hello_world_vishal.py
@@ -1,1 +1,9 @@
 
+def main():
+    print("Hello, World!")
+    print("This is Vishal contributing to Hacktoberfest 2025 ")
+    print("Keep learning, keep building, keep contributing ")
+
+if __name__ == "__main__":
+    main()
+
